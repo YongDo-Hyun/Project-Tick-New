817cc620bc886569545bf5807c3d245866d9d70f
Mehmet Samet Duman <yongdohyun@projecttick.org>
2026-03-01 17:28:35 +0300

    add Copyright holder in pax source files
    Signed-off-by: Mehmet Samet Duman <yongdohyun@projecttick.org>

--- options.c
+++ options.c
@@ -5,6 +5,9 @@
  * Copyright (c) 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
+ * Copyright (c) 2026
+ *  Project Tick. All rights reserved.
+ *
  * This code is derived from software contributed to Berkeley by
  * Keith Muller of the University of California, San Diego.
  *
